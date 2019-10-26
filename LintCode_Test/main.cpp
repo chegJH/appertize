44529ec84b4a34d7f0cda1d332c23c3a57afefbc
chegBaba <17byhejunjie@gmail.com>
2019-10-27 01:49:34 +0800

    Initial Commit

---
+++ LintCode_Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LintCode_Test
+//
+//  Created by Junjie He on 27/10/19.
+//  Copyright © 2019 Junjie He. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
